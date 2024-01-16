95ca8c39fc090831bfb0819a61df4b8a57121a45
OMFrydenlund <migfrydenlund@gmail.com>
2024-01-16 14:45:47 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChopShopV2
+{
+    public class Car
+    {
+        public bool IsInShop;
+        public bool EngineHealthy;
+        public bool BreaksHealthy;
+        public Car(bool isInShop, bool engineHealthy, bool breaksHealthy)
+        {
+            IsInShop = isInShop;
+            EngineHealthy = engineHealthy;
+            BreaksHealthy = breaksHealthy;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ChopShopV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            CarInspectionPrompt();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the shop!");
+            Console.ReadKey();
+        }
+
+        private static void CarInspectionPrompt()
+        {
+            Console.Clear();
+            Console.WriteLine("Would you like your car looked at?");
+            Console.WriteLine("Press Y or N: ");
+            GetInspectionResponse();
+        }
+
+        private static void GetInspectionResponse()
+        {
+            var userInput = Console.ReadLine();
+            if (userInput == null || userInput.ToUpper() != "Y" && userInput.ToUpper() != "N")
+            {
+                Console.WriteLine("\nThat isn't a valid option, press any button to continue...");
+                Console.ReadKey();
+                CarInspectionPrompt();
+            }
+            else if (userInput.ToUpper() == "Y")
+            {
+                Console.WriteLine("\nAlright let's take a look at your car.");
+            }
+            else if (userInput.ToUpper() == "N")
+            {
+                Console.WriteLine("\nAaaaalright then. Thanks for stopping by! >:)");
+            }
+        }
+    }
+}
